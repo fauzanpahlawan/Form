d7daf58b957008fda6fca850cb209f0f1a2e5843
fauzanpahlawan <fauzan.pahlawan@gmail.com>
2017-10-01 08:10:54 +0700

    Initial commit

---
+++ app/src/main/java/com/fauzan/form/FillFormActivity.java
@@ -0,0 +1,13 @@
+package com.fauzan.form;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FillFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill_form);
+    }
+}
---
+++ app/src/main/java/com/fauzan/form/ShowFormActivity.java
@@ -0,0 +1,4 @@
+package com.fauzan.form;
+
+public class ShowFormActivity {
+}
